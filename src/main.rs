6401c49889228a786ca40b0809ccc11e52796c4e
Isaac Grosof <isaacbg227@gmail.com>
2024-11-21 11:26:20 -0600

    Fixed bug in policy

--- src/main.rs
+++ src/main.rs
@@ -22,46 +22,82 @@ fn sim(num_jobs: u64, num_servers: usize, load: f64, seed: u64) -> f64 {
     let size_dist = Exp::new(1.0).unwrap();
     let mut next_arrival_time = arrival_dist.sample(&mut rng);
     while num_completions < num_jobs {
-        if false { //num_completions == 0 || num_completions.is_power_of_two() {
-            println!("{}: {:?}", num_completions, queue);
-        }
         queue.sort_by_key(|job| -n64(job.rem_size));
-        let num_in_service = if queue.len() <= num_servers {
-            queue.len()
+        if false {
+            println!(
+                "{}: {:?} {}",
+                time,
+                queue,
+                queue.iter().map(|job| job.rem_size).sum::<f64>()
+            );
+            std::io::stdin()
+                .read_line(&mut String::new())
+                .expect("Present");
+        }
+        let (num_in_service, first_sharing) = if queue.len() <= num_servers {
+            (queue.len(), queue.len().saturating_sub(1))
         } else {
             let service_threshold = queue[num_servers - 1].rem_size;
-            let mut out = num_servers;
+            let mut service_count = num_servers;
             for i in num_servers..queue.len() {
                 if queue[i].rem_size >= service_threshold - EPSILON {
-                    out += 1;
+                    service_count += 1;
                 } else {
                     break;
                 }
             }
-            out
+            let mut first_sharing = queue.len();
+            for i in 0..=num_servers {
+                if queue[i].rem_size <= service_threshold + EPSILON {
+                    first_sharing = i;
+                    break;
+                }
+            }
+            assert!(first_sharing < queue.len(), "First sharing updated");
+            (service_count, first_sharing)
         };
         let smallest_in_service = if num_in_service == 0 {
             INFINITY
         } else {
             queue[num_in_service - 1].rem_size
         };
+        let smallest_unshared = if first_sharing == 0 {
+            INFINITY
+        } else {
+            queue[first_sharing - 1].rem_size
+        };
         let smallest_out_of_service = if num_in_service == queue.len() {
             0.0
         } else {
             queue[num_in_service].rem_size
         };
-        let service_rate = num_in_service.max(num_servers);
+        let shared_service_rate = (num_servers.min(num_in_service) - first_sharing) as f64
+            / num_servers as f64
+            / (num_in_service - first_sharing) as f64;
         let service_duration = if queue.len() == 0 {
             INFINITY
+        } else if num_in_service <= num_servers {
+            (smallest_in_service - smallest_out_of_service) * num_servers as f64
         } else {
-            (smallest_in_service - smallest_out_of_service) * service_rate as f64
+            ((smallest_in_service - smallest_out_of_service) / shared_service_rate).min(
+                (smallest_unshared - smallest_in_service)
+                    / (1.0 / num_servers as f64 - shared_service_rate),
+            )
         };
         let event_duration = (next_arrival_time - time).min(service_duration);
         let was_arrival = event_duration < service_duration;
         time += event_duration;
+        let mut total_rate = 0.0;
         for i in 0..num_in_service {
-            queue[i].rem_size -= event_duration / service_rate as f64;
+            if i < first_sharing {
+                queue[i].rem_size -= event_duration / num_servers as f64;
+                total_rate += 1.0/num_servers as f64;
+            } else {
+                queue[i].rem_size -= event_duration * shared_service_rate;
+                total_rate += shared_service_rate;
+            }
         }
+        assert!(total_rate <= 1.0 + EPSILON);
         for i in (0..num_in_service).rev() {
             if queue[i].rem_size < EPSILON {
                 let _job = queue.remove(i);
@@ -83,11 +119,12 @@ fn sim(num_jobs: u64, num_servers: usize, load: f64, seed: u64) -> f64 {
 }
 
 fn main() {
-    let num_jobs = 10_000_000;
+    let num_jobs = 100_000_000;
     let num_servers = 2;
-    let seed = 0;
-    for load in vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95] {
-        let mean_work = sim(num_jobs, num_servers, load, seed);
-        println!("{}; {}", load, mean_work);
+    for seed in 0..10 {
+        for load in vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95] {
+            let mean_work = sim(num_jobs, num_servers, load, seed);
+            println!("{}; {}", load, mean_work);
+        }
     }
 }
